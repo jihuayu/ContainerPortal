7cb7f724b671261f0ca44a0e37b3f829b2587c2d
jihuayu <jihuayu123@gmail.com>
2024-08-10 18:45:28 +0800

    更新

--- mover.py
+++ mover.py
@@ -16,7 +16,11 @@ if __name__ == '__main__':
         print('Usage: python mover.py <repo>:<tag>')
         sys.exit(1)
     data = comment_issues(f"正在拉取镜像 `{sys.argv[1]}` 请稍后")
-    arg = sys.argv[1].split(':')
+    command_input = sys.argv[1].strip()
+    if command_input.count(':'):
+        arg = command_input.split(':')
+    else:
+        arg = command_input.split(' ')
     repo = arg[0]
     tag = len(arg) >= 2 and arg[1] or 'latest'
     image_url = move_image(client, repo, tag)
