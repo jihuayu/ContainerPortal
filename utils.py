35a1262a27581c982800fafed52a35658f50fcc5
jihuayu <jihuayu123@gmail.com>
2024-08-10 19:01:24 +0800

    autopep8

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 env_var_value = os.environ.get('TARGET_REPO')
 TARGET_REPO = env_var_value or 'registry.cn-hangzhou.aliyuncs.com/jihuayu/public'
 
+
 def move_image(client, repo, tag):
     new_tag = repo.replace('/', '-') + "-" + tag
     print(f'准备搬运 {repo}:{tag}->{new_tag}')
