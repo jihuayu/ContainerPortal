597cc4d6d79b95f3a7e346fc934c10e03bde547d
jihuayu <jihuayu123@gmail.com>
2024-08-10 16:52:00 +0800

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import os
+from typing import Dict, List
+
+import docker
+
+env_var_value = os.environ.get('TARGET_REPO')
+TARGET_REPO = env_var_value or 'registry.cn-hangzhou.aliyuncs.com/jihuayu/public'
+
+with open('config.json', 'r') as file:
+    data: Dict[str, List[str]] = json.load(file)
+
+client = docker.from_env()
+
+count = 0
+
+for repo, tags in data.items():
+    for tag in tags:
+        try:
+            new_tag = repo.replace('/', '-') + "-" + tag
+            print(f'准备搬运 {repo}:{tag}->{new_tag}')
+            image = client.images.pull(repo, tag)
+            image.tag(TARGET_REPO, new_tag)
+            client.images.push(TARGET_REPO, new_tag)
+            print(f"搬运 {repo}:{tag} 成功")
+            count = count + 1
+        except Exception as e:
+            print(f'搬运 {repo}:{tag} 时发生错误：', e)
+
+print("搬运完工，总计搬运镜像数量: ", count)
