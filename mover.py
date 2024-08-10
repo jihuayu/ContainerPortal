c8653620e7edf51cd7aa07a8f5855829e161ec76
jihuayu <jihuayu123@gmail.com>
2024-08-10 18:48:53 +0800

    更新

--- mover.py
+++ mover.py
@@ -16,13 +16,15 @@ if __name__ == '__main__':
         print('Usage: python mover.py <repo>:<tag>')
         sys.exit(1)
     data = comment_issues(f"正在拉取镜像 `{sys.argv[1]}` 请稍后")
-    command_input = sys.argv[1].strip()
-    if command_input.count(':'):
-        arg = command_input.split(':')
+
+    if len(sys.argv) >= 3:
+        repo = sys.argv[1].strip()
+        tag = sys.argv[2].strip()
     else:
-        arg = command_input.split(' ')
-    repo = arg[0]
-    tag = len(arg) >= 2 and arg[1] or 'latest'
+        arg = sys.argv[1].strip().split(':')
+        repo = arg[0]
+        tag = len(arg) >= 2 and arg[1] or 'latest'
+        
     image_url = move_image(client, repo, tag)
     edit_issue_comment(data.get('id'),
                        f"传输完毕，请运行以下指令拉取镜像 `{sys.argv[1]}`\n"
