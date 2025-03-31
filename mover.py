6acd0462a59dfb2feb4ccb523ae8f78201a525d1
纪华裕 <jihuayu123@gmail.com>
2025-03-31 03:08:17 +0000

    支持多平台镜像搬运，更新mover.py和utils.py以处理平台参数

--- mover.py
+++ mover.py
@@ -7,23 +7,35 @@ client = docker.from_env()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('Usage: python mover.py <repo>:<tag>')
+        print('Usage: python mover.py <repo>:<tag> [platform1,platform2,...]')
         sys.exit(1)
     data = comment_issues(f"正在拉取镜像 `{sys.argv[1]}` 请稍后")
 
     if len(sys.argv) >= 3:
         repo = sys.argv[1].strip()
         tag = sys.argv[2].strip()
+        # 如果有第4个参数，则认为是平台列表
+        platforms = sys.argv[3].strip().split(',') if len(sys.argv) >= 4 else ['linux/amd64']
     else:
         arg = sys.argv[1].strip().split(':')
         repo = arg[0]
         tag = len(arg) >= 2 and arg[1] or 'latest'
+        platforms = ['linux/amd64']
 
-    image_url = move_image(client, repo, tag)
-    edit_issue_comment(data.get('id'),
-                       f"传输完毕，请运行以下指令拉取镜像 `{sys.argv[1]}`\n"
-                       f"```shell\n"
-                       f"docker pull {image_url}\n"
-                       f"docker tag {image_url} {sys.argv[1]}\n"
-                       f"``` ")
+    image_urls = move_image(client, repo, tag, platforms)
+    
+    # 构建命令输出，支持多架构
+    pull_commands = []
+    tag_commands = []
+    for image_url in image_urls:
+        pull_commands.append(f"docker pull {image_url}")
+        # 为不同平台添加适当的标签
+        platform_part = image_url.split('-')[-1]  # 获取平台部分
+        tag_commands.append(f"docker tag {image_url} {repo}:{tag}")
+    
+    message = f"传输完毕，请运行以下指令拉取镜像 `{sys.argv[1]}`\n```shell\n"
+    message += "\n".join(pull_commands) + "\n"
+    message += "\n".join(tag_commands) + "\n```"
+    
+    edit_issue_comment(data.get('id'), message)
     close_issues()
--- utils.py
+++ utils.py
@@ -4,11 +4,12 @@ env_var_value = os.environ.get('TARGET_REPO')
 TARGET_REPO = env_var_value or 'registry.cn-hangzhou.aliyuncs.com/jihuayu/public'
 
 
-def move_image(client, repo, tag):
-    new_tag = repo.replace('/', '-') + "-" + tag
-    print(f'准备搬运 {repo}:{tag}->{new_tag}')
-    image = client.images.pull(repo, tag)
-    image.tag(TARGET_REPO, new_tag)
-    client.images.push(TARGET_REPO, new_tag)
-    print(f"搬运 {repo}:{tag} 成功")
-    return f'{TARGET_REPO}:{new_tag}'
+def move_image(client, repo, tag, platforms=['linux/amd64']):
+    for platform in platforms:
+        new_tag = repo.replace('/', '-') + "-" + tag + "-" + platform.replace('/', '-')
+        print(f'准备搬运 {repo}:{tag}->{new_tag} 平台: {platform}')
+        image = client.images.pull(repo, tag, platform=platform)
+        image.tag(TARGET_REPO, new_tag)
+        client.images.push(TARGET_REPO, new_tag)
+        print(f"搬运 {repo}:{tag} 平台 {platform} 成功")
+    return [f'{TARGET_REPO}:{repo.replace("/", "-")}-{tag}-{platform.replace("/", "-")}' for platform in platforms]
